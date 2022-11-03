e68e59c1fa55ac4e84b1ac85da86d0de817ac1de
Rodolfo Londero <rodolfo.londero@smarthis.com.br>
2022-11-03 09:40:58 -0300

    Creating server and first page

---
+++ web/pages/index.tsx
@@ -0,0 +1,18 @@
+
+export default function Home(props) {
+
+  return (
+    <h1>Contagem: {props.count}</h1>
+  )
+}
+
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3333/pools/count')
+  const data = await response.json()
+
+  return {
+    props: {
+      count: data.count
+    }
+  }
+}
