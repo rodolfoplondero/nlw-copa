f3fa52e524b182fe69092a6e916611467a89fc36
Rodolfo Londero <rodolfopl@gmail.com>
2022-11-05 19:59:05 -0300

    Rota de listagem de usuários

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,10 +1,21 @@
 import { FastifyInstance } from 'fastify';
 import { prisma } from '../lib/prisma';
+import { authenticate } from '../plugins/authenticate';
 
 export async function userRoutes(fastify: FastifyInstance) {
     fastify.get('/users/count', async () => {
         const count = await prisma.user.count();
 
         return { count };
+    });
+
+    fastify.get('/users', { onRequest: [authenticate] }, async (request) => {
+        const users = await prisma.user.findMany({
+            orderBy: {
+                name: 'asc'
+            }
+        });
+
+        return { users };
     });
 }
