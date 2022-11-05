76b93747b7eaf8a244ce8f7601362abab47a0a84
Rodolfo Londero <rodolfopl@gmail.com>
2022-11-05 15:57:13 -0300

    Implementação do login via Google

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,24 +1,28 @@
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { Loading } from './src/components/Loading';
-import { SignIn } from './src/screens/Signin';
+import { AuthContextProvider } from './src/contexts/AuthContext';
 
-import { THEME } from './src/styles/theme'
+import { Loading } from './src/components/Loading';
+import { SignIn } from './src/screens/SignIn';
+
+import { THEME } from './src/styles/theme';
+import { New } from './src/screens/New';
 
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <New /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
-
---
+++ mobile/src/screens/New.tsx
@@ -0,0 +1,11 @@
+import { VStack } from 'native-base';
+import { Header } from '../components/Header';
+
+export function New() {
+
+    return (
+        <VStack flex={1} bgColor="gray.900">
+            <Header title='Criar novo bolão' />
+        </VStack>
+    );
+}
